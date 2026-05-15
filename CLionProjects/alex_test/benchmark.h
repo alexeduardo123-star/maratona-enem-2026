ef7b96c3221a45dc0a9afb1ed453277dfb41228b
Alex Eduardo <alexeduardo123@unochapeco.edu.br>
2026-05-15 16:15:11 -0300

    Delete CLionProjects/alex_test directory

--- CLionProjects/alex_test/benchmark.h
+++
@@ -1,6 +0,0 @@
-#ifndef BENCHMARK_H
-#define BENCHMARK_H
-
-void executarBenchmark();
-
-#endif
--- CLionProjects/alex_test/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "benchmark.h"
-
-int main() {
-    printf("SISTEMA DE RANKING AVL x RB\n");
-    executarBenchmark();
-    return 0;
-}
